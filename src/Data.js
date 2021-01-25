b80583c7d03682f6230f6a989004790c0b3a51cc
celstuv <celstuv@yahoofr>
2021-01-25 18:40:31 +0100

    Data ok

---
+++ src/Data.js
@@ -0,0 +1,38 @@
+export const Contact = [
+  {
+    id: 1,
+    name: 'Helder L.',
+    Numero: '06 XX XX XX XX',
+    photo: '',
+  },
+  {
+    id: 2,
+    name: 'July B.',
+    Numero: '07 XX XX XX XX',
+    photo: '',
+  },
+  {
+    id: 3,
+    name: 'Sadio M.',
+    Numero: '06 XX XX XX XX',
+    photo: '',
+  },
+  {
+    id: 4,
+    name: 'Sandrine D.',
+    Numero: '06 XX XX XX XX',
+    photo: '',
+  },
+  {
+    id: 5,
+    name: 'Henriette S.',
+    Numero: '06 XX XX XX XX',
+    photo: '',
+  },
+  {
+    id: 6,
+    name: 'Nadia Z.',
+    Numero: '06 XX XX XX XX',
+    photo: '',
+  },
+]
